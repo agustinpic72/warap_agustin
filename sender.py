b4c0e413c37cec704cae279e9861a16e40161620
agustinpic72 <agustinpiccoli@gmail.com>
2023-02-06 11:14:17 -0300

    multiple images feature added

--- sender.py
+++ sender.py
@@ -1,26 +1,32 @@
 import pywhatkit
-from keyboard import press, release
+import glob
+from pywhatkit.core.core import copy_image
+from pyautogui import hotkey, press, keyDown, keyUp
 from time import sleep
 
 numeros = []
-with open('numeros.txt','r') as file:
+with open('numeros.txt', 'r') as file:
     numeros = file.readlines()
     numeros = [n[:-1] for n in numeros]
-with open('mensaje.txt','r') as file:
-    mensaje = file.read()
-
+with open('mensaje.txt', 'r') as file:
+    mensaje = file.read() <
 
 for numero in numeros:
-    pywhatkit.sendwhats_image(f"{numero}", "imagen.jpg", f"{mensaje}", wait_time=45)
+    pywhatkit.sendwhats_image(
+        f"{numero}", "imagenes/imagen1.jpeg", f"{mensaje}", wait_time=45)
+    for imagen in glob.glob('imagenes/*.jpeg'):
+        copy_image(imagen)
+        keyDown('ctrl')
+        press('v')
+        keyUp('ctrl')
+        sleep(3)
+    press('enter')
     print('closing tab...')
     sleep(10)
-    press('ctrl+w')
-    release('ctrl+w')
+    keyDown('ctrl')
+    press('w')
+    keyUp('ctrl')
     sleep(2)
     press('enter')
-    release('enter')
     sleep(2)
     print('releasing keys...')
-
-
-    
